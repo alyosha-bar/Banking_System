d7cfb4dd310b7c34bc3d659a4d58cecba8c63fbf
Alyosha Baryshnikov <alohahoy@gmail.com>
2023-05-04 17:44:24 +0100

    Adding basis of "Registration" system

--- src/Customer.java
+++ src/Customer.java
@@ -1,2 +1,49 @@
+
 public class Customer {
+
+    private String username;
+    private String password;
+    //possible PIN
+
+    private int bank_balance;
+
+    Customer(){
+
+    }
+
+    Customer(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getBank_balance() {
+        return bank_balance;
+    }
+
+    public void setBank_balance(int bank_balance) {
+        this.bank_balance = bank_balance;
+    }
+
+    public void changeBank_balance(int increment) {
+        this.bank_balance = this.bank_balance + increment;
+    }
+
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,157 @@
-package PACKAGE_NAME;public class Main {
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import javax.swing.JFrame;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Login (1) or new Account (2) ");
+        int choice = Integer.parseInt(sc.nextLine());
+
+        String[][] customers = readFile();
+
+        String[] usernames = new String[customers.length];
+        String[] passwords = new String[customers.length];
+        for(int i = 0; i<customers.length; i++) {
+            usernames[i] = customers[i][0];
+            passwords[i] = customers[i][1];
+        }
+
+
+
+        if (choice == 1) {
+            System.out.println("Username: ");
+            String user = sc.nextLine();
+
+            System.out.println("Password: ");
+            String pass = sc.nextLine();
+
+//            printArray(usernames, "Username");
+//            printArray(passwords, "Password");
+
+            boolean found = false;
+
+            for (int i = 0; i < usernames.length; i++) {
+                //checking username
+                if(user.equals(usernames[i])) {
+                    //checking password
+                    if (pass.equals(passwords[i])) {
+                        //both checks passed
+                        found = true;
+                        System.out.println("Access Granted");
+
+                        //OPEN NEW WINDOW
+//						JFrame frame = new JFrame("Logged In");
+//						frame.setVisible(true);
+                    }
+                    break;
+                }
+            }
+
+            if (found == false) {
+                System.out.println("Invalid Details");
+            }
+
+        }
+        else if (choice == 2) {
+            //write to file
+            System.out.println("Username: ");
+            String user = sc.nextLine();
+            System.out.println("Confirm: ");
+            String confirmUser = sc.nextLine();
+
+            System.out.println("\n");
+
+            System.out.println("Password: ");
+            String pass = sc.nextLine();
+            System.out.println("Confirm: ");
+            String confirmPass = sc.nextLine();
+
+            if(user.equals(confirmUser) && pass.equals(confirmPass)) {
+                System.out.println("Valid Details - adding account");
+            } else {
+                System.out.println("Invalid Details");
+            }
+
+
+        } else {
+            //something went wrong
+        }
+
+
+
+    }
+
+
+    public static void printArray(String[] arr, String prefix) {
+        for(int i = 0; i < arr.length; i++) {
+            System.out.println(prefix + " : "+arr[i]);
+        }
+        return;
+    }
+
+    public static boolean exitSystem() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Exit System? (y/n)");
+        String answer = sc.nextLine();
+        if (answer.equals("y")) {
+            return true;
+        }
+        return false;
+    }
+
+
+    public static String[][] readFile() throws IOException{
+        String filePath = "files/Customers.csv";
+        File file = new File(filePath);
+
+        List<String[]> rows = new ArrayList<>();
+
+        try (Scanner scanner = new Scanner(file)) {
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String[] values = line.split(",");
+                rows.add(values);
+            }
+        }
+
+        String username = "";
+        String password = "";
+        String balance = "";
+
+
+        String[][] customers = new String[rows.size()][3];
+
+        // Now you have a list of rows, each row represented as a String array
+        for (int i = 0; i < rows.size(); i++) {
+            String[] row = rows.get(i);
+
+            // Do something with each row
+            username = row[0];
+            password = row[1];
+            balance = row[2];
+
+//            System.out.println("Username: " + username);
+//            System.out.println("Password: " + password);
+//            System.out.println("Balance: " + balance + "\n");
+
+            //add the rows into some sort of 2D array
+            customers[i][0] = username;
+            customers[i][1] = password;
+            customers[i][2] = balance;
+        }
+
+
+
+        return customers;
+
+    }
+
 }
