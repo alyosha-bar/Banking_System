33297bb3c858585763abe7803cd5bfa28eee8c1f
Alyosha Baryshnikov <alohahoy@gmail.com>
2023-05-07 12:30:51 +0100

    Clean up Main.java

--- src/Main.java
+++ src/Main.java
@@ -8,69 +8,10 @@ import javax.swing.*;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-
+    public static void main(String[] args) {
+        //initialise dashboard
         JFrame dashboard = new Dashboard();
         dashboard.setVisible(true);
-
-
-//        Scanner sc = new Scanner(System.in);
-//
-//        System.out.println("Login (1) or new Account (2) ");
-//        int choice = Integer.parseInt(sc.nextLine());
-
-
-//        if (choice == 1) {
-//            //do nothing cus GUI exists
-//        }
-//        else if (choice == 2) {
-//            //write to file
-//            System.out.println("Username: ");
-//            String user = sc.nextLine();
-//            System.out.println("Confirm: ");
-//            String confirmUser = sc.nextLine();
-//
-//            System.out.println("\n");
-//
-//            System.out.println("Password: ");
-//            String pass = sc.nextLine();
-//            System.out.println("Confirm: ");
-//            String confirmPass = sc.nextLine();
-//
-//            if(user.equals(confirmUser) && pass.equals(confirmPass)) {
-//
-//                //add account to CSV
-//                String filePath = "files/Customers.csv";
-//                String[] userData1 = {user, pass, "0"};
-//
-//                try {
-//                    FileWriter fileWriter = new FileWriter(filePath, true);
-//
-//                    fileWriter.append("\n");
-//
-//                    // write user data
-//                    fileWriter.append(userData1[0]);
-//                    fileWriter.append(",");
-//                    fileWriter.append(userData1[1]);
-//                    fileWriter.append(",");
-//                    fileWriter.append(userData1[2]);
-//
-//
-//                    fileWriter.close();
-//                    System.out.println("Data written to CSV file successfully!");
-//                } catch (IOException e) {
-//                    System.err.println("Error writing data to CSV file: " + e.getMessage());
-//                }
-//
-//                System.out.println("Valid Details - adding account");
-//            } else {
-//                System.out.println("Invalid Details");
-//            }
-//        } else {
-//            //something went wrong
-//            System.out.println("Something went wrong");
-//        }
-
     }
 
     public static boolean checkDetails(String user, String pass) throws IOException {
@@ -100,7 +41,7 @@ public class Main {
             }
         }
 
-        if (found == false) {
+        if (!found) {
             System.out.println("Invalid Details");
         } else {
             //OPEN NEW WINDOW
@@ -111,23 +52,23 @@ public class Main {
         return found;
     }
 
-    public static void printArray(String[] arr, String prefix) {
-        for(int i = 0; i < arr.length; i++) {
-            System.out.println(prefix + " : "+arr[i]);
-        }
-        return;
-    }
+//    public static void printArray(String[] arr, String prefix) {
+//        for(int i = 0; i < arr.length; i++) {
+//            System.out.println(prefix + " : "+arr[i]);
+//        }
+//        return;
+//    }
 
-    public static boolean exitSystem() {
-        Scanner sc = new Scanner(System.in);
-
-        System.out.println("Exit System? (y/n)");
-        String answer = sc.nextLine();
-        if (answer.equals("y")) {
-            return true;
-        }
-        return false;
-    }
+//    public static boolean exitSystem() {
+//        Scanner sc = new Scanner(System.in);
+//
+//        System.out.println("Exit System? (y/n)");
+//        String answer = sc.nextLine();
+//        if (answer.equals("y")) {
+//            return true;
+//        }
+//        return false;
+//    }
 
     public static String[][] readFile() throws IOException{
         String filePath = "files/Customers.csv";
@@ -143,9 +84,9 @@ public class Main {
             }
         }
 
-        String username = "";
-        String password = "";
-        String balance = "";
+        String username;
+        String password;
+        String balance;
 
 
         String[][] customers = new String[rows.size()][3];
